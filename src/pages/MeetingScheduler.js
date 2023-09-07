23af57cbad3f6556b8bd211a049f87f483d783cb
Natalie Barnes <natalie.barnes@projectsynergyga.com>
2023-09-07 09:29:21 -0400

    [KAN-26-JC] added some logic to the main meeting scheduler component for getting the rooms and meetings from the database

--- src/pages/MeetingScheduler.js
+++ src/pages/MeetingScheduler.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 import Container from "../components/Layout/Container";
 import RoomColumn from "../components/Layout/RoomColumn/RoomColumn";
@@ -7,6 +9,34 @@ import MapColumn from "../components/Layout/MapColumn/MapColumn";
 
 const MeetingScheduler = (props) => {
   const navigate = useNavigate();
+  const roomsState = useSelector((state) => state.rooms);
+  const meetingsState = useSelector((state) => state.meetings);
+
+  const getRooms = async () => {
+    const res = await axios
+      .get("http://localhost:8080/rooms")
+      .then((res) => {
+        return res.data._embedded.rooms;
+      })
+      .catch((err) => {
+        return err;
+      });
+
+    return await res;
+  };
+
+  const getMeetings = async () => {
+    const res = await axios
+      .get("http://localhost:8080/meetings")
+      .then((res) => {
+        return res.data._embedded.meetings;
+      })
+      .catch((err) => {
+        return err;
+      });
+
+    return await res;
+  };
 
   useEffect(() => {
     // check if someone is logged in, if not then redirect
@@ -16,7 +46,28 @@ const MeetingScheduler = (props) => {
     if (!loggedInUser) {
       navigate("/landing");
     }
+
+    let rooms = getRooms();
+    let meetings = getMeetings();
+
+    console.log("ROOMS");
+    console.log(rooms);
+
+    console.log("MEETINGS");
+    console.log(meetings);
   });
+
+  useEffect(() => {
+    // get the rooms and meetings
+    let rooms = getRooms();
+    let meetings = getMeetings();
+
+    console.log("ROOMS2");
+    console.log(rooms);
+
+    console.log("MEETINGS2");
+    console.log(meetings);
+  }, [roomsState, meetingsState]);
 
   return (
     <div id="meeting-scheduler">
--- src/store/meetingStore.js
+++ src/store/meetingStore.js
@@ -1,15 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  roomAvailable: false,
+  roomAvailability: {
+    A: false,
+    B: false,
+    C: false,
+    D: false,
+    E: false,
+    F: false,
+    G: false,
+    H: false,
+    I: false,
+    J: false,
+  },
+  meetings: {},
+  rooms: {},
 };
 
 const meetingSlice = createSlice({
   name: "rooms",
   initialState,
   reducers: {
-    changeAvailability(state) {
-      state.roomAvailable = false;
+    changeAvailability(state, payload) {
+      // The payload here will be the name of the room: A, B, C, etc.
+      let newAvailibility = state.roomAvailability;
+
+      newAvailibility[payload] = !state.roomAvailability[payload]; // set the value of the room availability to the opposite of it's original value
+
+      state.roomAvailability = newAvailibility; // update the state array with the new changed array
     },
   },
 });
